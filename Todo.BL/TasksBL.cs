0299765a7344333944a589077b77af178921aa95
Ran Wahle <ran.wahle@gmail.com>
2015-08-18 14:51:19 +0300

    Set default task status to new

--- Todo.BL/TasksBL.cs
+++ Todo.BL/TasksBL.cs
@@ -12,12 +12,23 @@ namespace Todo.BL
     {
         public static IEnumerable<TodoTask> GetTasks()
         {
-            return TasksDAL.GetTasks();
+            var tasks = TasksDAL.GetTasks();
+
+            foreach (var item in tasks)
+            {
+                if (item.Status == default(TodoTaskStatus))
+                {
+                    item.Status = TodoTaskStatus.New;
+                }
+            }
+
+            return tasks;
         }
 
         public static void CreateTask(TodoTask task)
         {
             task.CreatedDate = DateTime.Now;
+            task.Status = TodoTaskStatus.New;
             TasksDAL.CreateTask(task);
         }
     }
